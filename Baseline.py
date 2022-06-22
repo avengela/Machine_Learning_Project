30bb1358df9257761129d855e60c34ee8d055d8c
avengela <angelikabulkowska@gmail.com>
2022-06-23 00:49:00 +0200

    Cleaning, baseline update, metrics update

--- Baseline.py
+++ Baseline.py
@@ -1,64 +1,38 @@
 def main():
-    from sklearn import svm, datasets
-    from sklearn.dummy import DummyClassifier
     import pandas as pd
-    from numpy import genfromtxt
-    import pandas as pd
-    from sklearn.metrics import f1_score
-    from sklearn.model_selection import train_test_split
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import roc_curve
-    from sklearn.metrics import accuracy_score
     import matplotlib.pyplot as plt
-    from sklearn.metrics import plot_confusion_matrix
 
+    from sklearn.model_selection import train_test_split
+    from sklearn.dummy import DummyClassifier
+
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import ConfusionMatrixDisplay
+    from sklearn.metrics import balanced_accuracy_score
 
     test_data = pd.read_csv("data/test_data.csv", header=None)
     train_data = pd.read_csv("data/train_data.csv", header=None)
     train_labels = pd.read_csv("data/train_labels.csv", header=None)
 
-    dummy = DummyClassifier(strategy="most_frequent")
-    dummy.fit(train_data,train_labels)
+    seed = 43
 
-    X_train, X_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.25, random_state=seed)
+    # baseline
+    X_train, X_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.5, random_state=seed)
 
-    y_predict = dummy.predict(X_train)
-    df = pd.DataFrame(y_predict)
-    dummy.score(X_train, y_train)
+    dummy_clf = DummyClassifier(strategy="stratified")
+    dummy_clf.fit(X_train, y_train)
+    y_predict = dummy_clf.predict(X_test)
+    sc = dummy_clf.score(X_test, y_predict)
+    print(f"Dummy classifier score: {sc}")
 
-    f1_score(X_train, y_train)
-
-    plot_confusion_matrix(dummy, train_data, y_train)
+    # metric checked on baseline
+    ConfusionMatrixDisplay.from_estimator(dummy_clf, y_test, y_predict)
     plt.show()
 
-    accuracy_score(X_train, y_predict)
+    f1 = f1_score(y_test, y_predict)
+    print(f"f1 score: {f1:.3f}")
 
-    fpr, tpr, thresholds = roc_curve(X_train, y_predict)
-
-    plt.figure(figsize=(15, 7))
-    plt.plot(fpr, tpr, alpha=0.5, color="blue", label="Elements")
-    plt.title("ROC Curve", fontsize=20)
-    plt.xlabel("False Positive Rate", fontsize=16)
-    plt.ylabel("True Positive Rate", fontsize=16)
-    plt.legend()
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    balanced_accuracy = balanced_accuracy_score(y_test, y_predict)
+    print(f"Balanced accuracy: {balanced_accuracy:.3f}")
 
 if __name__ == '__main__':
     main()

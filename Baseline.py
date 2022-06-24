2b9c5e5e54eff61cb0656cb7992419e347c78125
avengela <angelikabulkowska@gmail.com>
2022-06-24 14:16:08 +0200

    Adding stratify to train_test_split, PEP 8

--- Baseline.py
+++ Baseline.py
@@ -2,15 +2,13 @@ def main():
     import pandas as pd
     import matplotlib.pyplot as plt
     import numpy as np
-    import random
 
     from sklearn.model_selection import train_test_split
     from sklearn.dummy import DummyClassifier
 
     from sklearn.metrics import f1_score
     from sklearn.metrics import ConfusionMatrixDisplay
-
-
+    from sklearn.metrics import classification_report
 
     train_data = pd.read_csv("data/train_data.csv", header=None)
     train_labels = pd.read_csv("data/train_labels.csv", header=None)
@@ -20,24 +18,28 @@ def main():
 
     seed = np.random.seed(147)
 
-
-    def baseline_cals(train_data,train_labels):
-        # baseline
-        X_train, X_test, y_train, y_test = train_test_split(train_data, train_labels, test_size=0.25, random_state=seed)
+    def baseline_cals(training_x: np.array or pd.DataFrame, training_y: np.array or pd.DataFrame) -> None:
+        """
+        Uses DummyClassifier on training set
+        Shows Confusion Matrix, F1 score and classification_report
+        """
+        X_train, X_test, y_train, y_test = train_test_split(training_x, training_y, test_size=0.25, random_state=seed,
+                                                            stratify=training_y)
 
         dummy_clf = DummyClassifier(strategy="stratified")
         dummy_clf.fit(X_train, y_train)
-        y_predict = dummy_clf.predict(X_test)
-        sc = dummy_clf.score(X_test, y_predict)
+        y_pred = dummy_clf.predict(X_test)
+        sc = dummy_clf.score(X_test, y_pred)
         print(f"Dummy classifier score: {sc}")
 
         # metric checked on baseline
-        ConfusionMatrixDisplay.from_estimator(dummy_clf, y_test, y_predict)
+        ConfusionMatrixDisplay.from_estimator(dummy_clf, y_test, y_pred)
         plt.show()
 
-        f1 = f1_score(y_test, y_predict)
+        f1 = f1_score(y_test, y_pred)
         print(f"f1 score: {f1:.3f}")
 
+        print(classification_report(y_test, y_pred))
 
     print("Data baseline")
     baseline_cals(train_data, train_labels)
